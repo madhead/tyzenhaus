94329a51f4351e13c90623ec1feb322a4405aff7
madhead <siarhei.krukau@gmail.com>
2026-07-11 02:16:52 +0200

    Better wiring

--- launcher/fly/src/main/kotlin/me/madhead/tyzenhaus/launcher/fly/koin/pipeline.kt
+++ launcher/fly/src/main/kotlin/me/madhead/tyzenhaus/launcher/fly/koin/pipeline.kt
@@ -3,6 +3,7 @@ package me.madhead.tyzenhaus.launcher.fly.koin
 import dev.inmo.tgbotapi.types.chat.ExtendedBot
 import me.madhead.tyzenhaus.core.debts.DebtsCalculator
 import me.madhead.tyzenhaus.core.telegram.updates.UpdateProcessingPipeline
+import me.madhead.tyzenhaus.core.telegram.updates.UpdateProcessor
 import me.madhead.tyzenhaus.core.telegram.updates.expense.AmountReplyUpdateProcessor
 import me.madhead.tyzenhaus.core.telegram.updates.expense.ConfirmationCancelCallbackQueryUpdateProcessor
 import me.madhead.tyzenhaus.core.telegram.updates.expense.ConfirmationOKCallbackQueryUpdateProcessor
@@ -22,6 +23,7 @@ import me.madhead.tyzenhaus.core.telegram.updates.lang.LangCommandUpdateProcesso
 import me.madhead.tyzenhaus.core.telegram.updates.policy.PrivacyCommandUpdateProcessor
 import me.madhead.tyzenhaus.core.telegram.updates.supergroup.SupergroupChatCreatedUpdateProcessor
 import me.madhead.tyzenhaus.core.telegram.updates.support.IDCommandUpdateProcessor
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val pipelineModule = module {
@@ -35,66 +37,66 @@ val pipelineModule = module {
             requestsExecutor = get(),
             groupConfigRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         StartCommandUpdateProcessor(
             requestsExecutor = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         HelpCommandUpdateProcessor(
             requestsExecutor = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         IDCommandUpdateProcessor(
             requestsExecutor = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         ExpenseCommandUpdateProcessor(
             requestsExecutor = get(),
             dialogStateRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         DebtsCommandUpdateProcessor(
             requestsExecutor = get(),
             balanceRepository = get(),
             debtsCalculator = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         AmountReplyUpdateProcessor(
             requestsExecutor = get(),
             dialogStateRepository = get(),
             groupCurrenciesService = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         CurrencyReplyUpdateProcessor(
             requestsExecutor = get(),
             dialogStateRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         TitleReplyUpdateProcessor(
             requestsExecutor = get(),
             dialogStateRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         ParticipantCallbackQueryUpdateProcessor(
             requestsExecutor = get(),
             dialogStateRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         DoneCallbackQueryUpdateProcessor(
             requestsExecutor = get(),
             dialogStateRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         ConfirmationOKCallbackQueryUpdateProcessor(
             requestsExecutor = get(),
@@ -103,72 +105,53 @@ val pipelineModule = module {
             transactionRepository = get(),
             balanceRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         ConfirmationCancelCallbackQueryUpdateProcessor(
             requestsExecutor = get(),
             dialogStateRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         LangCommandUpdateProcessor(
             requestsExecutor = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         LangCallbackQueryUpdateProcessor(
             requestsExecutor = get(),
             groupConfigRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         ParticipateCommandUpdateProcessor(
             requestsExecutor = get(),
             groupConfigRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         SupergroupChatCreatedUpdateProcessor(
             supergroupRepository = get(),
             meterRegistry = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         HistoryCommandUpdateProcessor(
             requestsExecutor = get(),
             me = get(),
             apiTokenRepository = get(),
         )
-    }
+    } bind UpdateProcessor::class
     single {
         PrivacyCommandUpdateProcessor(
             requestsExecutor = get(),
             me = get(),
         )
-    }
+    } bind UpdateProcessor::class
+
     single {
         UpdateProcessingPipeline(
-            listOf(
-                get<WelcomeMessageUpdateProcessor>(),
-                get<StartCommandUpdateProcessor>(),
-                get<HelpCommandUpdateProcessor>(),
-                get<IDCommandUpdateProcessor>(),
-                get<ExpenseCommandUpdateProcessor>(),
-                get<DebtsCommandUpdateProcessor>(),
-                get<AmountReplyUpdateProcessor>(),
-                get<CurrencyReplyUpdateProcessor>(),
-                get<TitleReplyUpdateProcessor>(),
-                get<ParticipantCallbackQueryUpdateProcessor>(),
-                get<DoneCallbackQueryUpdateProcessor>(),
-                get<ConfirmationOKCallbackQueryUpdateProcessor>(),
-                get<ConfirmationCancelCallbackQueryUpdateProcessor>(),
-                get<LangCommandUpdateProcessor>(),
-                get<LangCallbackQueryUpdateProcessor>(),
-                get<ParticipateCommandUpdateProcessor>(),
-                get<SupergroupChatCreatedUpdateProcessor>(),
-                get<HistoryCommandUpdateProcessor>(),
-                get<PrivacyCommandUpdateProcessor>(),
-            ),
+            getAll<UpdateProcessor>(),
             get(),
             get(),
         )
